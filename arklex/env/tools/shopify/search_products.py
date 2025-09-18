b5a08b9a8379077e0b9a6c6f5441a88111d6f6b4
xinyang-articulatei <xinyang.wang@articulateai.com>
2025-09-18 10:48:12 -0400

    fix llm config in product search

--- arklex/env/tools/shopify/search_products.py
+++ arklex/env/tools/shopify/search_products.py
@@ -126,11 +126,8 @@ def search_products(
                 }
                 card_list.append(product_dict)
             if card_list:
-                if not kwargs.get("llm_config"):
-                    raise ValueError("llm_provider must be explicitly specified")
-
                 model_service: ModelService = ModelService(
-                    LLMConfig.model_validate(kwargs["llm_config"])
+                    LLMConfig.model_validate(kwargs)
                 )
                 message = f"You are helping a customer search products based on the query and get results below and those results will be presented using product card format.\n\n{json.dumps(card_list)}\n\nGenerate a response to continue the conversation without explicitly mentioning contents of the search result. Include one or two questions about those products to know the user's preference. Keep the response within 50 words.\nDIRECTLY GIVE THE RESPONSE."
                 answer = model_service.get_response(message)
