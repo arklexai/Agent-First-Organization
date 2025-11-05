28ca2afc3e7f17273939f8a295c74429bafbe228
Saravanan1999 <46484761+Saravanan1999@users.noreply.github.com>
2025-11-05 18:22:49 -0500

    optional possible tags

--- arklex/resources/workers/milvus_rag/entities.py
+++ arklex/resources/workers/milvus_rag/entities.py
@@ -15,7 +15,7 @@ class MilvusRAGWorkerData(BaseModel):
     version: str
     collection_name: str
     tags: dict[str, Any]
-    possible_tags: dict[str, list[str]]
+    possible_tags: dict[str, list[str]] | None = None
 
 
 class MilvusRAGWorkerOutput(WorkerOutput):
